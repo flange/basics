346eea45488754d035d4c060caf5b883eda60b3e
Frank Lange <langefrq@informatik.hu-berlin.de>
2017-03-13 18:01:03 +0100

    added Java dir and first example: Arrays

---
+++ java/00-array/Main.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    int[] ia1 = new int[5];
+    int[] ia2 = {1, 2, 3, 4, 5};
+
+    System.out.println(Arrays.toString(ia1));
+    System.out.println(Arrays.toString(ia2));
+
+
+    System.out.println();
+
+
+    Integer[] ia3 = new Integer[5];
+    Integer[] ia4 = {1, 2, 3, 4, 5};
+
+    System.out.println(Arrays.toString(ia3));
+    System.out.println(Arrays.toString(ia4));
+
+    return;
+  }
+}
